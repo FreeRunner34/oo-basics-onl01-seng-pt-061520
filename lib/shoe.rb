de57980cf8f6a8885c5bbf3e1a4b295d08206bae
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-06 04:20:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+
+class Book
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
